bbb63b01f81e741952687de5ea51d95fdea8f290
mamiavi <minambresvidal@gmail.com>
2025-11-20 18:32:18 +0100

    Fixed bug video can't be reproduced again once played.

--- photos/assets/js/gallery.js
+++ photos/assets/js/gallery.js
@@ -3,6 +3,7 @@ const lightbox = document.getElementById('lightbox');
 const addButton = document.getElementById('add-button');
 const closeBtn = document.getElementById('closeBtn');
 const galleryImages = document.querySelectorAll('.gallery-img');
+const galleryVideos = document.querySelectorAll('.gallery-video');
 
 let swiper = null;
 
@@ -13,9 +14,27 @@ galleryImages.forEach(img => {
     });
 });
 
+galleryVideos.forEach(vid => {
+    vid.addEventListener('click', () => {
+    const index = parseInt(vid.dataset.index, 10);
+    openLightbox(index);
+    });
+});
+
+function resetVideos() {
+    const allVideos = document.querySelectorAll('.swiper-slide video');
+    allVideos.forEach(video => {
+        video.pause();
+        video.currentTime = 0;
+        video.load();
+    });
+}
+
 function openLightbox(index) {
     lightbox.classList.add('active');
     addButton.classList.add('hidden');
+
+    resetVideos();
 
     if (!swiper) {
     swiper = new Swiper('.swiper', {
@@ -29,6 +48,11 @@ function openLightbox(index) {
         el: '.swiper-pagination',
         clickable: true,
         },
+        on: {
+            slideChange: () => {
+                resetVideos();
+            },
+        }
     });
     } else {
     swiper.slideTo(index, 0);
@@ -43,6 +67,7 @@ function closeLightbox() {
     if (history.state?.lightboxOpen) {
     history.back();
     }
+    resetVideos();
 }
 
 closeBtn.addEventListener('click', closeLightbox);
