2bff4c7f273354c4d4a7ed582efdee9ea35387eb
Manuel Miñambres <58034475+mamiavi@users.noreply.github.com>
2025-11-20 16:35:06 +0100

    New countdown page (#10)
    * First approach to the countdown page.
    
    * Don't block admin views, no matter which date has been set
    
    * Added the date time too, not just the date.
    
    * Added more text in the countdown view

--- photos/urls.py
+++ photos/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("countdown/", views.countdown_page, name="countdown"),
     path('local_upload/', views.local_upload, name='local_upload'),
     path('gallery/', views.show_gallery, name='gallery'),
     path('get_upload_url/', views.get_upload_url, name='get_url'),
